1f621b9e6628f69fdf9cb2ab644154e9c52f979f
Pavithra Sivasamy <pavithrasivasamy0@gmail.com>
2023-06-30 04:50:01 +0530

    Working on opening and closing of modals

---
+++ Modal Window/script.js
@@ -0,0 +1,28 @@
+"use strict";
+const showModalBtn = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener("click", openModal);
+}
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});

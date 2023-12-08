d39eb898aa18d742cd92cf9247d1acc5e3231790
Pavithra Sivasamy <pavithrasivasamy0@gmail.com>
2023-12-08 11:05:01 +0530

    initial commit of getCountry app using XMLHTtpRequest type

---
+++ Async Practice/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+//old way of raising request using XMLHttpRequest calls
+const renderCountry = data => {
+  const [languages] = Object.values(data.languages);
+  const [currency] = Object.values(data.currencies);
+  const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${languages}</p>
+      <p class="country__row"><span>💰</span>${currency.name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+    const [neighbors] = data.borders;
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbors}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data] = JSON.parse(this.responseText);
+      renderCountry(data);
+    });
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
